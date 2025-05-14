235b09a4130c078427e33f62a7d682893546c053
Mateus Lemos <teuse853@gmail.com>
2025-05-14 20:29:37 -0300

    feat: create user entity

--- src/main/java/br/mateus/feed/model/User.java
+++ src/main/java/br/mateus/feed/model/User.java
@@ -1,11 +1,18 @@
 package br.mateus.feed.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Set;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -13,7 +20,42 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "users")
-public class User {
+public class User implements Serializable {
 
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 100)
+    @Email
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String bio;
+
+    @Column(name = "proile_picture_url")
+    private String profiePictureUrl;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime created_at;
+
+    @OneToMany(mappedBy = "author")
+    private List<Post> posts;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_followers",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "follower_id")
+    )
+    private Set<User> followers;
 }
