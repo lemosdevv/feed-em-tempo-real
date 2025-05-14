1d8cb3eeceeb1dfe5a0db8874532e16551fbbbe3
Mateus Lemos <teuse853@gmail.com>
2025-05-14 20:50:44 -0300

    feat: create like entity

---
+++ src/main/java/br/mateus/feed/model/Like.java
@@ -0,0 +1,47 @@
+package br.mateus.feed.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "likes")
+public class Like implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private LikeId id;
+
+    @ManyToOne
+    @MapsId("postId")
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @MapsId("userId")
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Embeddable
+    public class LikeId implements Serializable{
+        private Long postId;
+        private Long userId;
+    }
+}
