c98682a161b4d0afcf2c4131550ca046433e46eb
Mateus Lemos <teuse853@gmail.com>
2025-05-14 20:12:49 -0300

    feat: first commit

---
+++ src/main/java/br/mateus/feed/model/User.java
@@ -0,0 +1,19 @@
+package br.mateus.feed.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    private Long id;
+}
